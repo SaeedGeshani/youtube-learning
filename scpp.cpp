51273d707e28f064ca2f49d33924e398c442f53e
SaeedGeshani <154725043+SaeedGeshani@users.noreply.github.com>
2023-12-30 12:35:32 +0330

    a code with capability to 2 times the numbers you give to it

---
+++ scpp.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+
+	int n;
+	cout << "enter a number" << endl;
+	cin >> n;
+	cout << "your 2 times number is" << endl;
+	cout << 2*n;
+
+	return 0
+}
+
